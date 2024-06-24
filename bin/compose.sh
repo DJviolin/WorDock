a045ab7ae89e9ff8b9fe4a24488686e280f89534
István Lantos <lantosistvan89@gmail.com>
2024-06-24 13:14:18 +0200

    2024. 06. 24. 13:14:18,59

--- bin/compose.sh
+++ bin/compose.sh
@@ -106,7 +106,7 @@ bootstrap_fn() {
 	# Catch-All domain to Apache
 	docker compose exec php-fpm sh -c "set -e \
 		&& mkdir -p $PROJECT_CONTAINER_DIR/$SERVER_NAME \
-		&& echo \"<?php phpinfo(); ?>\" > $PROJECT_CONTAINER_DIR/phpinfo.php \
+		&& echo \"<?php phpinfo(); ?>\" > $PROJECT_CONTAINER_DIR/$SERVER_NAME/phpinfo.php \
 		&& echo '<html><body><h1>It works!</h1></body></html>' > $PROJECT_CONTAINER_DIR/$SERVER_NAME/index.html \
 		&& chown -R $USER_NAME:$USER_NAME $PROJECT_CONTAINER_DIR/$SERVER_NAME \
 		&& mkdir -p $dir \
